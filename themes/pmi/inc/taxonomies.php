31f97e88476cc907a101a8444ccf750af395b738
ANKUSH <ankush7377@gmail.com>
2015-06-23 22:08:49 +0530

    taxonomy name improved

--- themes/pmi/inc/taxonomies.php
+++ themes/pmi/inc/taxonomies.php
@@ -37,35 +37,8 @@
 			register_taxonomy( 'marcas', 'productos', $args );
 		}
 		
-		// Tipo de producto
-		if( ! taxonomy_exists('tipo_de_producto')){
-
-			$labels = array(
-				'name'              => 'Tipo de producto',
-				'singular_name'     => 'Tipo de producto',
-				'search_items'      => 'Buscar',
-				'all_items'         => 'Todos',
-				'edit_item'         => 'Editar Tipo de producto',
-				'update_item'       => 'Actualizar Tipo de producto',
-				'add_new_item'      => 'Nueva Tipo de producto',
-				'new_item_name'     => 'Nombre nuevo Tipo',
-				'menu_name'         => 'Tipo de producto'
-			);
-			$args = array(
-				'hierarchical'      => true,
-				'labels'            => $labels,
-				'show_ui'           => true,
-				'show_admin_column' => true,
-				'show_in_nav_menus' => true,
-				'query_var'         => true,
-				'rewrite'           => array( 'slug' => 'tipo_de_producto' ),
-			);
-
-			register_taxonomy( 'tipo_de_producto', 'productos', $args );
-		}
-
 		// Producto utilizado
-		if( ! taxonomy_exists('producto_utilizado')){
+		if( ! taxonomy_exists('producto-utilizado')){
 
 			$labels = array(
 				'name'              => 'Producto utilizado',
@@ -85,13 +58,12 @@
 				'show_admin_column' => true,
 				'show_in_nav_menus' => true,
 				'query_var'         => true,
-				'rewrite'           => array( 'slug' => 'producto_utilizado' ),
+				'rewrite'           => array( 'slug' => 'producto-utilizado' ),
 			);
 
-			register_taxonomy( 'producto_utilizado', 'recetas', $args );
+			register_taxonomy( 'producto-utilizado', 'recetas', $args );
 		}
 		insert_term_brand();
-		insert_tipo_de_producto();
 		insert_producto_utilizado();
 	}// custom_taxonomies_callback
 
@@ -104,11 +76,6 @@
 		// }
 	}// insert_term_brands
 
-	function insert_tipo_de_producto(){
-		// if ( ! term_exists( 'Nike', 'brand' ) ){
-		// 	wp_insert_term( 'Nike', 'brand' );
-		// }
-	}// insert_tipo_de_producto
 	function insert_producto_utilizado(){
 		// if ( ! term_exists( 'Nike', 'brand' ) ){
 		// 	wp_insert_term( 'Nike', 'brand' );
