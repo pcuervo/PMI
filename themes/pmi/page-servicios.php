67cf2900716a39b58ffd1475589403cef9a84db8
maichomper <maichomper@gmail.com>
2015-06-23 10:35:34 -0500

    clean page servicios

--- themes/pmi/page-servicios.php
+++ themes/pmi/page-servicios.php
@@ -3,17 +3,11 @@
 	the_post();
 	
 	$cover_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-	$nosotros_info_query = new WP_Query( 'pagename=servicios' );
-	if ( $nosotros_info_query->have_posts() ) : $nosotros_info_query->the_post(); 
-	?>
-	    <h2 class="[ sub-title ] [ ]"><?php the_title() ?></h2>
-	<?php endif;
-	wp_reset_query();
-	?>
+?>
+	<h2 class="[ sub-title ] [ ]"><?php the_title() ?></h2>
 	<div class="[ bg-image ] [ margin-bottom--large ]" style="background-image: url(<?php echo $cover_url[0] ?>)">
 		<?php the_content(); ?>
 	</div>
-
 
 <?php
 	get_footer();
