b798c16e2ee0b261d219aea718a9cc5392826351
Raúl De Zamacona <raul@zamacona.me>
2015-06-24 18:16:05 -0500

    some basic style shit

--- themes/pmi/header.php
+++ themes/pmi/header.php
@@ -3,24 +3,27 @@
 		<meta charset="utf-8">
 		<title><?php print_title(); ?></title>
 
-		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-180x180.png">
-		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-194x194.png" sizes="194x194">
-		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/android-chrome-192x192.png" sizes="192x192">
-		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="<?php echo THEMEPATH; ?>images/favicon/manifest.json">
-		<meta name="msapplication-TileColor" content="#fc3a1e">
-		<meta name="msapplication-TileImage" content="<?php echo THEMEPATH; ?>images/favicon/mstile-144x144.png">
-		<meta name="theme-color" content="#fc3a1e">
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-60x60.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo THEMEPATH; ?>images/favicon/apple-touch-icon-152x152.png" />
+		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-196x196.png" sizes="196x196" />
+		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="<?php echo THEMEPATH; ?>images/favicon/favicon-128.png" sizes="128x128" />
+		<meta name="application-name" content="&nbsp;"/>
+		<meta name="msapplication-TileColor" content="#FFFFFF" />
+		<meta name="msapplication-TileImage" content="<?php echo THEMEPATH; ?>images/favicon/mstile-144x144.png" />
+		<meta name="msapplication-square70x70logo" content="<?php echo THEMEPATH; ?>images/favicon/mstile-70x70.png" />
+		<meta name="msapplication-square150x150logo" content="<?php echo THEMEPATH; ?>images/favicon/mstile-150x150.png" />
+		<meta name="msapplication-wide310x150logo" content="<?php echo THEMEPATH; ?>images/favicon/mstile-310x150.png" />
+		<meta name="msapplication-square310x310logo" content="<?php echo THEMEPATH; ?>images/favicon/mstile-310x310.png" />
+
 
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
@@ -28,7 +31,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="cleartype" content="on">
 		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-		<script src="//use.typekit.net/yms0cif.js"></script>
+		<script src="//use.typekit.net/nso1tmf.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
 		<?php wp_head(); ?>
 	</head>
