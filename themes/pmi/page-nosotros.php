22c374bfdfc8324ce0c90f8b39fb88c77cfecacd
ANKUSH <ankush7377@gmail.com>
2015-06-24 09:56:59 +0530

    nosotors page UI cone

--- themes/pmi/page-nosotros.php
+++ themes/pmi/page-nosotros.php
@@ -4,11 +4,19 @@
 	
 	$cover_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 ?>
-	<h2 class="[]"><?php the_title() ?></h2>
-	<img src="<?php echo $cover_url[0] ?>" />
-	<div class="[ margin-bottom--large ]">
-		<?php the_content(); ?>
-	</div>
+	<section class="[ margin-bottom--large ]">
+		<div class="[ wrapper ]">
+			<div class="[ row ]">
+				<h2 class="[ text-center ]"><?php the_title() ?></h2>
+				<div class="[ columna xmall-12  ][ center ][ margin-bottom ]">
+					<img src="<?php echo $cover_url[0] ?>" class="[ image-responsive ]" />
+				</div>
+				<div class="[ columna xmall-12 medium-8  ][ center ][ margin-bottom ]">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php
 	get_footer();
