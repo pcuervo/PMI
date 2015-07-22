a57e1814919df60960e41d43f523db4b5fd611c1
Raúl De Zamacona <raul@zamacona.me>
2015-07-22 18:52:16 -0500

    Single procut desktop

--- themes/pmi/single-productos.php
+++ themes/pmi/single-productos.php
@@ -19,7 +19,7 @@
 
 	<!-- BANNER -->
 	<section class="[]">
-		<div class="[ hidden--large ]">
+		<div class="[ hidden--xmedium ]">
 			<div class="[ bg-image ][ margin-bottom ]" style="background-image: url('<?php echo $product_bg_img_url; ?>')">
 				<div class="[ wrapper ]">
 					<img class="[ block center ][ xmall-12 small-8 medium-6 xmedium-4 ][ padding--large ]" src="<?php echo $product_img_url[0] ?>">
@@ -46,39 +46,45 @@
 			</div><!-- wrapper -->
 		</div><!-- hidden-large -->
 
-		<div class="[ shown--large ]">
-			<h2> <?php the_title() ?> </h2>
-			<p> <?php echo $text_banner ?> </p>
-			<div class="[ row ]">
-				<div class="[ columna xmall-12 ][ text-center ]">
-					<div class="[ columna xmall-6 ]">
-						<p><?php echo $net_content ?></p>
-						<p>cont. net.</p>
-					</div>
-					<div class="[ columna xmall-6 ]">
-						<p><?php echo $product_portions ?></p>
-						<p>porciones</p>
-					</div>
-				</div>
-				<div class="[ span xmall-12 medium-6 ] [ padding ]">
-					<div class="[ bg-light ] [ relative ]">
-						<img src="<?php echo $product_img_url[0] ?>" class="[ image-responsive ] [ margin-bottom ]">
-					</div>
+		<div class="[ shown--xmedium ]">
+			<div class="[ bg-image ][ margin-bottom ]" style="background-image: url('<?php echo $product_bg_img_url; ?>')">
+				<div class="[ diagonal-light-blue-to-transparecy ]">
+					<div class="[ wrapper ]">
+						<div class="[ row ][ padding ]">
+							<div class="[ columna xmall-6 ]">
+								<h2 class="[ light ][ margin-bottom ]"> <?php the_title() ?></h2>
+								<p class="[ light ][ margin-bottom ]"> <?php echo $text_banner ?></p>
+								<div class="[ border-light ][ xmall-6 ]">
+									<div class="[ row ][ padding--small ]">
+										<div class="[ columna xmall-6 ]">
+											<p class="[ text-center ][ light ][ no-margin ]"><?php echo $net_content ?></p>
+											<p class="[ small-text text-center ][ light ]">cont. net.</p>
+										</div>
+										<div class="[ columna xmall-6 ]">
+											<p class="[ text-center ][ light ][ no-margin ]"><?php echo $product_portions ?></p>
+											<p class="[ small-text text-center ][ light ]">porciones</p>
+										</div>
+									</div>
+								</div><!-- row -->
+							</div>
+							<div class="[ columna xmall-6 ]">
+								<img class="[ block center ][ xmedium-10 ][ padding--large ]" src="<?php echo $product_img_url[0] ?>">
+							</div>
+						</div><!-- row -->
+					</div><!-- wrapper -->
 				</div>
 			</div>
 		</div><!-- shown-large -->
 	</section><!-- BANNER -->
 
 	<!-- PRODUCT INFO -->
-	<section class="[  ]">
+	<section class="[ margin-bottom--large ]">
 		<div class="[ wrapper ]">
-
-			<div class="[ columna xmall-12 medium-6 ][ center ][ margin-bottom ]">
-				<?php the_content() ?>
-			</div>
-
 			<div class="[ row ]">
-				<div class="[ columna xmall-12 medium-6 ]">
+				<div class="[ columna xmall-12 xmedium-6 ][ margin-bottom ]">
+					<?php the_content() ?>
+				</div>
+				<div class="[ columna xmall-12 xmedium-6 ]">
 					<?php
 					if( ! empty( $indications ) ) :
 						echo '<h3 class="[ secondary ][ no-margin ]">Indicaciones</h3>';
@@ -99,7 +105,7 @@
 					endif;
 					?>
 				</div>
-			</div>
+			</div><!-- row -->
 		</div>
 	</section><!-- PRODUCT INFO -->
 
@@ -107,12 +113,12 @@
 	<?php if( ! empty( $recommended_recipe ) ) : ?>
 		<section>
 			<div class="[ wrapper ]">
-				<h2 class="[ text-center ][ margin-bottom ][ uppercase ]">Receta recomendada</h2>
+				<h2 class="[ title ][ text-center ][ margin-bottom ][ uppercase ]">Receta recomendada</h2>
 
 				<div class="[ center xmedium-8 ]">
 					<div class="[ row ]">
 
-						<div class="[ post ][ columna xmall-12 xmedium-6 ][ margin-bottom ]">
+						<div class="[ post ][ columna xmall-12 small-6 ][ margin-bottom ]">
 							<div class="[ drop-shadow ]">
 								<a href="<?php echo $recommended_recipe['permalink'] ?>">
 									<img src="<?php echo $recommended_recipe['image_url'] ?>" class="[ image-responsive ] [ margin-bottom ]'" />
@@ -123,8 +129,7 @@
 							</div>
 						</div>
 
-						<div class="[ post ][ columna xmall-12 xmedium-6 ][ margin-bottom--large ]">
-
+						<div class="[ post ][ columna xmall-12 small-6 ][ margin-bottom--large ]">
 							<div class="[ gradient-border ][ diagonal-green-to-blue-gradient ][ margin-bottom ]">
 								<span class="[ block ][ bg-light ][ padding--small ]">
 									<div class="[ row ]">
@@ -169,15 +174,15 @@
 	<?php if( ! empty( $similar_products ) ) : ?>
 		<section class="[ margin-bottom--large ]">
 			<div class="[ wrapper ]">
-				<h2 class="[ title ][ text-center ][ padding ][ margin-bottom ]">Productos relacionados</h2>
+				<h2 class="[ title ][ text-center ][ margin-bottom ][ uppercase ]">Productos similares</h2>
 				<div class="[ row ]">
 					<?php foreach ( $similar_products as $product ) : ?>
-						<div class="[ post ][ columna xmall-12 small-6 medium-4 ][ margin-bottom--large ]">
+						<div class="[ post ][ columna xmall-12 medium-6 xmedium-4 ][ margin-bottom--large ]">
 							<div class="[ drop-shadow ]">
 								<a href="<?php echo $product['permalink'] ?>">
-									<img src="<?php echo $product['product_img_url'][0] ?>" alt="<?php echo $product['title'] ?>" class="[ image-responsive ] [ margin-bottom ]">
+									<img src="<?php echo $product['product_img_url'][0] ?>" alt="<?php echo $product['title'] ?>" class="[ image-responsive ]">
 									<div class="[ padding--small ][ bg-primary ]">
-										<p class="[ post-title ] [ ]"><?php echo $product['title'] ?></p>
+										<p class="[ post-title ][ ]"><?php echo $product['title'] ?></p>
 									</div>
 								</a>
 							</div>
