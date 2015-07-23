f0d21ecfb3f1bc4dcb51132980db9e2f05a27b11
Raúl De Zamacona <raul@zamacona.me>
2015-07-22 20:39:23 -0500

    opinión de lso expertos y pages y contacto

--- themes/pmi/single-opiniones-expertos.php
+++ themes/pmi/single-opiniones-expertos.php
@@ -1,15 +1,19 @@
-<?php 
+<?php
 	get_header();
 	the_post();
-	
+
 	$cover_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 ?>
 	<section class="[ margin-bottom--large ]">
 		<div class="[ wrapper ]">
 			<div class="[ row ]">
 				<h2 class="[ title ][ text-center ][ padding ][ margin-bottom ]"><?php the_title() ?></h2>
-				<div class="[ columna xmall-12  ][ center ][ margin-bottom ]">
-					<img src="<?php echo $cover_url[0] ?>" class="[ image-responsive ]" />
+				<div class="[ bg-image ][ margin-bottom ]" style="background-image: url('<?php echo $cover_url[0] ?>')">
+					<div class="[ diagonal-green-to-blue-gradient ][ padding--large ]">
+						<span class="[ block ][ padding--large ]">&nbsp;</span><br />
+						<span class="[ padding--large ][ shown--large ]">&nbsp;</span><br />
+						<span class="[ padding--large ][ shown--large ]">&nbsp;</span><br />
+					</div>
 				</div>
 				<div class="[ columna xmall-12 medium-8  ][ center ][ margin-bottom ]">
 					<?php the_content(); ?>
